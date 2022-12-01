7d862b80b9a0caf00900e7ff72655a5fe4c576df
Sunny Kumar <119514322+SunnyKumar-iiitt@users.noreply.github.com>
2022-12-01 10:54:23 +0530

    Add files via upload

---
+++ welcome_file.cpp
@@ -0,0 +1,28 @@
+#include<iostream>
+#include<conio.h>
+#include<windows.h>
+#include<unistd.h>
+using namespace std;
+void welcome();
+void intro();
+
+
+
+  void welcome()
+  {
+
+  cout<<"\n\n\n\n\n\n\n ";
+
+  cout<<"\t\t------------ WELCOME TO -----------"<<endl;
+sleep(2);
+
+  intro();
+ sleep(2);
+
+  }
+
+ void intro()
+ {
+
+  cout<<"\t\t############### Sun Rise Bank #################"<<endl;
+ }
